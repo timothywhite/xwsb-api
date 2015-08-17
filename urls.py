72dc246309ad0140d2abb60727c19d5ec0570d95
tim <timford88@gmail.com>
2015-08-17 23:12:11 +0000

    Streamline API

--- urls.py
+++ urls.py
@@ -2,30 +2,17 @@ from django.conf.urls import url, include
 from squad_builder import views
 from rest_framework.routers import DefaultRouter
 
-# Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'action', views.ActionViewSet)
-router.register(r'maneuver', views.ManeuverViewSet)
-router.register(r'ship/maneuver', views.ShipManeuverViewSet)
 router.register(r'ship', views.ShipViewSet)
-router.register(r'pilot/slot', views.PilotSlotViewSet)
 router.register(r'pilot', views.PilotViewSet)
-router.register(r'upgrade/bonus/requirement', views.UpgradeBonusRequirementViewSet)
-router.register(r'upgrade/bonus', views.UpgradeBonusViewSet)
-router.register(r'upgrade/requirement', views.UpgradeRequirementViewSet)
-router.register(r'upgrade/maneuver', views.UpgradeManeuverViewSet)
 router.register(r'upgrade/type', views.UpgradeTypeViewSet)
 router.register(r'upgrade', views.UpgradeViewSet, base_name='Upgrade')
-router.register(r'expansion/ship', views.ExpansionShipViewSet)
-router.register(r'expansion/pilot', views.ExpansionPilotViewSet)
-router.register(r'expansion/upgrade', views.ExpansionUpgradeViewSet)
 router.register(r'expansion', views.ExpansionViewSet)
-router.register(r'squad/pilot/upgrade', views.SquadPilotUpgradeViewSet)
-router.register(r'squad/pilot', views.SquadPilotViewSet)
+router.register(r'squad/pilot/upgrade', views.SquadPilotUpgradeViewSet, base_name='SquadPilotUpgrade')
+router.register(r'squad/pilot', views.SquadPilotViewSet, base_name='SquadPilot')
 router.register(r'squad', views.SquadViewSet, base_name='Squad')
-# The API URLs are now determined automatically by the router.
-# Additionally, we include the login URLs for the browsable API.
+
 urlpatterns = [
-	url(r'^', include(router.urls)),
-	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    url(r'^', include(router.urls)),
 ]
